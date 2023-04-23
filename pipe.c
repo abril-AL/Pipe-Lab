c568858058c6255ed475f4afb5cc2e6dea9344b1
Abril Aguilar-Lopez <abril@lnxsrv13.seas.ucla.edu>
2023-04-23 15:24:41 -0700

    initial commit

---
+++ pipe.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main (int argc, char *argv[])
+{
+  if ( argc < 2 ) {
+    return 1;
+  }
+  for ( int i = 1; i < argc; i++ ) {
+    int fd[2];
+    if ( pipe(fd) == -1 ) { return 1; }
+    pid_t cid = fork();
+    if ( cid == 0 ){
+      char cmd[] = argv[i];
+      char * cArg = {NULL};
+      char * const cEnvr = {NULL};
+      execve(argv[i],cArg,cEnvr);
+    }
+    waitpid(cid,NULL,0);
+  }
+}
