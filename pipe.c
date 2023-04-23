27c2b5d37d111feb0d7b6c96c5c1e6a84b470b8e
Abril Aguilar Lopez <abril124@ucla.edu>
2023-04-23 16:31:01 -0700

    track pids

--- pipe.c
+++ pipe.c
@@ -10,11 +10,14 @@ int main (int argc, char *argv[])
   if ( argc < 2 ) {
     return 1;
   }
+  pid_t *pid_array = NULL;
+  int pid_count = 0;
   if (argc == 2 )
   {
     //just one cmd, can just go dirtectly to console, no pipe
 
   }
+  //create ID here
   for ( int i = 1; i < argc; i++ ) {
     int fd[2];
     if ( pipe(fd) == -1 ) { return 1; }
@@ -24,6 +27,19 @@ int main (int argc, char *argv[])
         return errno;
       }
     }
-    waitpid(cid,NULL,0);
+    else{
+      pid_count++;
+      pid_array = realloc(pid_array, pid_count * sizeof(pid_t));
+      pid_array[pid_count-1] = child_pid;
+    }
   }
+    waitpid(pid_array[0],NULL,0);
+  // Print all child process PIDs
+  printf("Child process PIDs:\n");
+  for (i = 0; i < pid_count; i++) {
+      printf("%d\n", pid_array[i]);
+  }
+
+  // Free memory
+  free(pid_array);
 }
