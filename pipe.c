97f180913ef082a92aa3faf784edeef152e0540c
Abril Aguilar Lopez <abril124@ucla.edu>
2023-04-23 21:28:42 -0700

    try..

--- pipe.c
+++ pipe.c
@@ -5,7 +5,7 @@
 #include <sys/wait.h>
 #include <errno.h>
 
-int main2 (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
   if ( argc < 2 ) {
     return 0;
@@ -23,58 +23,37 @@ int main2 (int argc, char *argv[])
 
   //more than 1 cmd ( need pipes )
   int fd[2];
-  for ( int i = 1; i < argc; i++ ) {
+  int i;
+  for ( i = 1; i < argc-1; i++ ) {
     if( i == 1 ){//first child - input from parent’s stdin
       if ( pipe(fd) == -1 ) { return 1; }//[0] read [1] write
       pid_t cid = fork();
       if ( cid == 0 ){ // child call execlp
         close(fd[0]);
-        dup2(fd[1],STDOUT_FILENO);
+        dup2(fd[1],1);
         int err = execlp(argv[i], argv[i], (char *) NULL);   if (err == -1) { return errno; };
-      }else{
-        close(fd[1]);//close write
       }
+      dup2(pd[0], 0);//prev child output into input
+      close(fd[1]);//close write
     }
-    if (i == argc-1){//last child - outputs to 
+    /* 
+     if (i == argc-1){//last child - outputs to 
       //wont use new pipe
       pid_t cid = fork();
       if ( cid == 0 ){ // child call execlp
-        dup2(fd[0],STDIN_FILENO);
+        dup2(fd[0],0);
         if (execlp(argv[i], argv[i], (char *) NULL) == -1) { return errno; }
       }else{
         close(fd[0]);//close read fd (end)
       }
-    }
-    /* 
+     }
+     
       else{//child in between
       //get read and write fd and set to  stdin and stdout}
     */
+    //run last cmd
+    int err = execlp(argv[i], argv[i], (char *) NULL);   if (err == -1) { return errno; };
   }
 
   return 0;
-}
-
-int main (int argc, char ** argv) {
-    int i;
-
-    for( i=1; i<argc-1; i++)
-    {
-        int pd[2];
-        pipe(pd);
-
-        if (!fork()) {
-            dup2(pd[1], 1); // remap output back to parent
-            execlp(argv[i], argv[i], NULL);
-            perror("exec");
-            abort();
-        }
-
-        // remap output from previous child to input
-        dup2(pd[0], 0);
-        close(pd[1]);
-    }
-
-    execlp(argv[i], argv[i], NULL);
-    perror("exec");
-    abort();
 }
