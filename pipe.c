672aff0ce06a3cbe52f22bcc4ee18c723c952723
Abril Aguilar Lopez <abril124@ucla.edu>
2023-04-23 20:25:24 -0700

    fd stuff fixed

--- pipe.c
+++ pipe.c
@@ -22,48 +22,42 @@ int main (int argc, char *argv[])
   }
 
   //more than 1 cmd ( need pipes )
-  pid_t *pid_array = NULL; int pid_count = 0; //for later waiting 
+  //pid_t *pid_array = NULL; int pid_count = 0; //for later waiting 
   int fd[2];
   for ( int i = 1; i < argc; i++ ) {
     if( i == 1 ){//first child - input from parent’s stdin
       if ( pipe(fd) == -1 ) { return 1; }//[0] read [1] write
       pid_t cid = fork();
       if ( cid == 0 ){ // child call execlp
-        printf("first child\n");
         close(fd[0]);
         dup2(fd[1],STDOUT_FILENO);
-        if (execlp(argv[i], argv[i], (char *) NULL) == -1) { return errno; }
+        int err = execlp(argv[i], argv[i], (char *) NULL);   if (err == -1) { return errno; };
       }else{
-        pid_count++; pid_array = realloc(pid_array, pid_count * sizeof(pid_t)); pid_array[pid_count-1] = cid;
+        //pid_count++; pid_array = realloc(pid_array, pid_count * sizeof(pid_t)); pid_array[pid_count-1] = cid;
         close(fd[1]);//close write
-        printf("wut");
       }
     }
-    if (i == argc-1){//last child - outputs to , wont use new pipe ( close? )
-      printf("%d \n",i);
+    if (i == argc-1){//last child - outputs to 
+      //wont use new pipe
       pid_t cid = fork();
       if ( cid == 0 ){ // child call execlp
-        printf("last child\n");
-        dup2(fd[1],STDIN_FILENO);
-        printf("%s\n",argv[i]);
+        dup2(fd[0],STDIN_FILENO);
+        //printf("%s\n",argv[i]);
         if (execlp(argv[i], argv[i], (char *) NULL) == -1) { return errno; }
       }else{
-        pid_count++; pid_array = realloc(pid_array, pid_count * sizeof(pid_t)); pid_array[pid_count-1] = cid;
+        //pid_count++; pid_array = realloc(pid_array, pid_count * sizeof(pid_t)); pid_array[pid_count-1] = cid;
         close(fd[0]);//close read fd (end)
-        printf("lhere");
       }
-        printf("2here");
     }
     /* 
       else{//child in between
       //get read and write fd and set to  stdin and stdout}
     */
-    printf("loop");
   }
 
-  /*
-  //wait for all processes
-  for (int i = 0; i < argc-1; i++) {
+  /*  
+   //wait for all processes
+   for (int i = 0; i < argc-1; i++) {
     int status;
     pid_t result = waitpid(pid_array[i], &status, 0);
 
@@ -72,13 +66,13 @@ int main (int argc, char *argv[])
     } else if (WIFEXITED(status)) {
       //printf("Child process %d with PID %d exited with status %d\n", i+1, pid_array[i], WEXITSTATUS(status));
     }
-  }
-  // Print all child process PIDs  -  printf("Child process PIDs:\n");
-  for (int i = 0; i < pid_count; i++) {
+   }
+   // Print all child process PIDs  -  printf("Child process PIDs:\n");
+   for (int i = 0; i < pid_count; i++) {
     //printf("%d\n", pid_array[i]);
-  }
+   }
   */
-  // Free memory
-  free(pid_array);
+  // Free memory, note, dont need to wait for exec processes (?)
+  //free(pid_array);
   return 0;
 }
