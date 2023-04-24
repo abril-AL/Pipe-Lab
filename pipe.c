e42b86c1ef9b845ed0a866c5753ca0a2cbc43986
Abril Aguilar Lopez <abril124@ucla.edu>
2023-04-23 21:36:59 -0700

    .

--- pipe.c
+++ pipe.c
@@ -29,31 +29,18 @@ int main (int argc, char *argv[])
       if ( pipe(fd) == -1 ) { return 1; }//[0] read [1] write
       pid_t cid = fork();
       if ( cid == 0 ){ // child call execlp
-        close(fd[0]);
         dup2(fd[1],1);
-        int err = execlp(argv[i], argv[i], (char *) NULL);   if (err == -1) { return errno; };
+        close(fd[0]);
+        int err = execlp(argv[i], argv[i], (char *) NULL);
+        if (err == -1) { return errno; };
       }
       dup2(fd[0], 0);//prev child output into input
       close(fd[1]);//close write
     }
-    /* 
-     if (i == argc-1){//last child - outputs to 
-      //wont use new pipe
-      pid_t cid = fork();
-      if ( cid == 0 ){ // child call execlp
-        dup2(fd[0],0);
-        if (execlp(argv[i], argv[i], (char *) NULL) == -1) { return errno; }
-      }else{
-        close(fd[0]);//close read fd (end)
-      }
-     }
-     
-      else{//child in between
-      //get read and write fd and set to  stdin and stdout}
-    */
-    //run last cmd
-    int err = execlp(argv[i], argv[i], (char *) NULL);   if (err == -1) { return errno; };
+    
   }
-
+  //run last cmd
+  int err = execlp(argv[i], argv[i], (char *) NULL);   
+  if (err == -1) { return errno; };
   return 0;
 }
